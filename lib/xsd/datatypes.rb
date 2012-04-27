83146d09d07e092f7bf320076054596ccf749d7e
Joe Rafaniello <jrafanie@gmail.com>
2012-04-27 18:41:38 -0400

    Fix undefined method jd_to_ajd on ruby 1.9.

--- lib/xsd/datatypes.rb
+++ lib/xsd/datatypes.rb
@@ -540,7 +540,7 @@ module XSDDateTimeImpl
   end
 
   def to_date
-    Date.new!(@data.class.send(:jd_to_ajd, @data.jd, 0, 0), 0, @data.start)
+    Date.jd(@data.jd, @data.start)
   end
 
   def to_datetime
