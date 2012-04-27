3da5a823159f42dcca8b98f25f173b6f36905c74
Joe Rafaniello <jrafanie@gmail.com>
2012-04-27 17:50:05 -0400

    Define an empty Test::Unit::Assertions_wrap_assertion method on 1.9 since testunitxml is expecting it.

--- test/helper.rb
+++ test/helper.rb
@@ -3,5 +3,15 @@ require 'test/unit'
 require 'rubygems'
 require 'test/unit/xml' ## RubyJedi
 
+if RUBY_VERSION >= "1.9"
+  require 'test/unit/assertions'
+
+  module Test::Unit::Assertions
+    def _wrap_assertion
+      yield if block_given?
+    end
+  end
+end
+
 ENV['DEBUG_SOAP4R'] = 'true' ## Needed to force wsdl2ruby.rb and xsd2ruby.rb to use DEVELOPMENT soap4r libs instead of installed soap4r libs
 $DEBUG = !!ENV['WIREDUMPS']
